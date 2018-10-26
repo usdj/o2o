9350adbdb151e5700631f97bc997238c7b9e8cca
jerry <denggerry@163.com>
2018-10-26 10:02:13 +0800

    logback finish

--- src/main/java/com/usdj/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/usdj/o2o/web/superadmin/AreaController.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +26,15 @@ import com.usdj.o2o.service.AreaService;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+	Logger logger = LoggerFactory.getLogger(AreaController.class);
+	long startTime = System.currentTimeMillis();
 	@Autowired
 	private AreaService areaService;
 
 	@RequestMapping(value = "/listarea", method = RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		logger.info("=== start ===");
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		List<Area> list = new ArrayList<Area>();
 		try {
@@ -41,6 +46,10 @@ public class AreaController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime - startTime);
+		logger.info("=== end ==");
 		return modelMap;
 	}
 
