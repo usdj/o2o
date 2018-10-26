1b23ba8fcc7319545329c8869f7467a83681f28d
jerry <denggerry@163.com>
2018-10-26 10:55:50 +0800

    dao finish

---
+++ src/main/java/com/usdj/o2o/dao/ShopDao.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.usdj.o2o.dao;
+
+import com.usdj.o2o.entity.Shop;
+
+/**
+ * @author gerrydeng
+ *
+ */
+public interface ShopDao {
+	int insertShop(Shop shop);
+	int updateShop(Shop shop);
+}
